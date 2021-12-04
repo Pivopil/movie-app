8b68971d49c5c1646cea9345682e5a8a5a1c0079
Pivopil <myworkplace8@gmail.com>
2021-12-04 18:23:20 +0200

    feat: Generated maven config for the app

---
+++ src/main/java/io/github/pivopil/movieapp/MovieApp.java
@@ -0,0 +1,13 @@
+package io.github.pivopil.movieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApp.class, args);
+	}
+
+}
