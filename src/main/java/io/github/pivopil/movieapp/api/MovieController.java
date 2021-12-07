a25d20d0a380a9a582f014551438033106b2ddd5
Pivopil <myworkplace8@gmail.com>
2021-12-07 12:58:54 +0200

    feat: Validation for movie id added for set rating score request

--- src/main/java/io/github/pivopil/movieapp/api/MovieController.java
+++ src/main/java/io/github/pivopil/movieapp/api/MovieController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.security.Principal;
 import java.util.List;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class MovieController {
@@ -23,7 +26,9 @@ public class MovieController {
 
   @PatchMapping(value = "/api/movies/{movieId}/rating")
   public void createOrUpdateMovieRating(
-      Principal principal, @PathVariable Long movieId, @Validated @RequestBody RatingScore ratingScore) {
+      Principal principal,
+      @PathVariable @NotNull @Min(1) Long movieId,
+      @RequestBody @Validated RatingScore ratingScore) {
     movieService.createOrUpdateMovieRating(principal.getName(), movieId, ratingScore.getScore());
   }
 }
--- src/main/java/io/github/pivopil/movieapp/handler/MovieAppExceptionHandler.java
+++ src/main/java/io/github/pivopil/movieapp/handler/MovieAppExceptionHandler.java
@@ -9,14 +9,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ValidationException;
+
 @Slf4j
 @ControllerAdvice
 public class MovieAppExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(value = {RuntimeException.class})
   protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-    log.error(ex.getMessage(), ex);
-    return handleExceptionInternal(
-        ex, "Unhandled exception", new HttpHeaders(), HttpStatus.CONFLICT, request);
+    HttpStatus status = HttpStatus.CONFLICT;
+    String message = "Unhandled exception";
+    if (ex instanceof ValidationException) {
+      status = HttpStatus.BAD_REQUEST;
+      message = ex.getMessage();
+      log.error(message);
+    } else {
+      log.error(message, ex);
+    }
+    return handleExceptionInternal(ex, message, new HttpHeaders(), status, request);
   }
 }
